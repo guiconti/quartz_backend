c2c6999f5d359e53204a5eb69295767e9b37b77a
GuiMac <guibasconti@gmail.com>
2019-01-27 14:28:58 -0200

    Reset players crystal on explosion

--- server/utils/playerExploded.js
+++ server/utils/playerExploded.js
@@ -9,6 +9,7 @@ module.exports = (game, playerIndex) => {
   game.players[playerIndex].hasAnIdiotBook = true;
   for (let i = 0; i < game.cave.crystals.length; i++) {
     game.cave.crystals[i].amount += game.players[playerIndex].crystals[i].amount;
+    game.players[playerIndex].crystals[i].amount = 0;
   }
   return game;
 };
