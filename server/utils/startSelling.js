d687ec7c3713e974f3862ac208eb372cfc7fc6e7
Guilherme Conti <guibasconti@gmail.com>
2019-06-29 20:16:33 -0300

    Send push when selling starts

--- server/utils/startSelling.js
+++ server/utils/startSelling.js
@@ -3,8 +3,19 @@
  * @module utils/startSelling
  */
 
+const push = require('./push');
+const constants = require('./constants');
+
 module.exports = game => {
   game.isSelling = true;
+  const payload = {
+    title: constants.messages.push.selling.title,
+    body: constants.messages.push.selling.body,
+    icon: '/static/icon-192x192.png',
+    data: {
+      url: `https://quartz.tiimus.com/games/${String(game._id)}`
+    }
+  };
   for (let i = 0; i < game.players.length; i++) {
     if (game.players[i].keptCrystal && game.players[i].keptCrystal !== '') {
       for (let j = 0; j < game.players[i].crystals.length; j++) {
@@ -15,5 +26,6 @@ module.exports = game => {
         }
       }
     }
+    push(game, i, payload);
   }
 };
